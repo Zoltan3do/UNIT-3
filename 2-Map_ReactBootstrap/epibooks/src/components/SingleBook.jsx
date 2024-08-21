f6b209a0d2a8920b9fee494ca66468823c8e97de
Zoltan3do <baronemanuel05@gmail.com>
2024-08-21 18:30:12 +0200

    style no

--- 2-Map_ReactBootstrap/epibooks/src/components/SingleBook.jsx
+++ 2-Map_ReactBootstrap/epibooks/src/components/SingleBook.jsx
@@ -11,7 +11,7 @@ class SingleBook extends Component {
     render() {
         return (
             <Col xs={12} md={6} lg={4} xl={3} xxl={2} className="my-2 z-3">
-                <Card style={{ width: '100%' }} className={`h-100 d-flex flex-column ${this.state.selected ? 'transformationScale' : ''} z-2 mt-5`}>
+                <Card className={`w-100 h-100 d-flex flex-column ${this.state.selected ? 'transformationScale' : ''} z-2 mt-5`}>
                     <Card.Img variant="top" src={this.props.book.img} onClick={() => this.setState({ selected: !this.state.selected })} />
                     <Card.Body className="d-flex flex-column">
                         <div className="flex-grow-1">
