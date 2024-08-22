78545c3f396bc867abede26791b4da48b6db52be
Zoltan3do <baronemanuel05@gmail.com>
2024-08-22 18:05:13 +0200

    addComment

--- 2-Map_ReactBootstrap/epibooks/src/components/SingleBook.jsx
+++ 2-Map_ReactBootstrap/epibooks/src/components/SingleBook.jsx
@@ -1,5 +1,6 @@
 import { Col, Card } from "react-bootstrap";
 import { Component } from "react";
+import CommentArea from "./CommentArea";
 
 
 class SingleBook extends Component {
@@ -10,13 +11,14 @@ class SingleBook extends Component {
 
     render() {
         return (
-            <Col xs={12} md={6} lg={4} xl={3} xxl={2} className="my-2 z-3">
-                <Card className={`w-100 h-100 d-flex flex-column ${this.state.selected ? 'transformationScale' : ''} z-2 mt-5`}>
+            <Col xs={12} md={6} lg={4} xl={3} xxl={2} className="my-2">
+                <Card className={`w-100 h-100 d-flex flex-column ${this.state.selected ? 'transformationScale z-3' : ''} z-2 mt-5`}>
                     <Card.Img variant="top" src={this.props.book.img} onClick={() => this.setState({ selected: !this.state.selected })} />
                     <Card.Body className="d-flex flex-column">
                         <div className="flex-grow-1">
                             <Card.Title>{this.props.book.title}</Card.Title>
                         </div>
+                        {this.state.selected && <CommentArea bookId={this.props.book.asin}/>}
                     </Card.Body>
                 </Card>
             </Col>
