4350b5bcbb17ee5404dca4541fbc96d65e685630
Zoltan3do <baronemanuel05@gmail.com>
2024-09-06 21:30:01 +0200

    try to fix

--- 2-React_Advanced/netflix-clone/src/components/MovieDetails.jsx
+++ 2-React_Advanced/netflix-clone/src/components/MovieDetails.jsx
@@ -7,7 +7,7 @@ function MovieDetails() {
     const [data1, setData1] = useState({});
     const [data2, setData2] = useState({});
 
-    const params = useParams();
+    const { params } = useParams();
 
     function fetchFilms() {
         fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=e8c1f24e&s/imdbID/${params.movieId}`)
@@ -29,7 +29,8 @@ function MovieDetails() {
     function fetchComments() {
         fetch(`https://striveschool-api.herokuapp.com/api/comments/${params.movieId}`, {
             headers: {
-                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNGViNmYyNjBjYzAwMTVjYzBkY2QiLCJpYXQiOjE3MjU2NDQ4NzAsImV4cCI6MTcyNjg1NDQ3MH0.e0_OTv1d4lkiPGRMkSk8wZIuwie6zJ5RDAL00fgz37I"
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNGViNmYyNjBjYzAwMTVjYzBkY2QiLCJpYXQiOjE3MjU2NDQ4NzAsImV4cCI6MTcyNjg1NDQ3MH0.e0_OTv1d4lkiPGRMkSk8wZIuwie6zJ5RDAL00fgz37I",
+                "Content-type": "application/json"
             }
         })
             .then(response => {
@@ -51,11 +52,12 @@ function MovieDetails() {
     useEffect(() => {
         fetchFilms();
         fetchComments()
+        console.log(data2)
     }, [params.movieId])
 
 
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '18rem' }} className="d-flex flex-column align-items-center">
             <Card.Img variant="top" src={data1.Poster} />
             <Card.Body>
                 <Card.Title>{data1.Title}</Card.Title>
