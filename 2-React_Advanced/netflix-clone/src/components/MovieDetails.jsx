d4809a8d871d18463f16138ca3798aef7e7d36d9
Zoltan3do <baronemanuel05@gmail.com>
2024-09-06 21:37:09 +0200

    fixed

--- 2-React_Advanced/netflix-clone/src/components/MovieDetails.jsx
+++ 2-React_Advanced/netflix-clone/src/components/MovieDetails.jsx
@@ -7,10 +7,10 @@ function MovieDetails() {
     const [data1, setData1] = useState({});
     const [data2, setData2] = useState({});
 
-    const { params } = useParams();
+    const { movieId } = useParams();
 
     function fetchFilms() {
-        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=e8c1f24e&s/imdbID/${params.movieId}`)
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=e8c1f24e&s/imdbID/${movieId}`)
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -27,7 +27,7 @@ function MovieDetails() {
     };
 
     function fetchComments() {
-        fetch(`https://striveschool-api.herokuapp.com/api/comments/${params.movieId}`, {
+        fetch(`https://striveschool-api.herokuapp.com/api/comments/${movieId}`, {
             headers: {
                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNGViNmYyNjBjYzAwMTVjYzBkY2QiLCJpYXQiOjE3MjU2NDQ4NzAsImV4cCI6MTcyNjg1NDQ3MH0.e0_OTv1d4lkiPGRMkSk8wZIuwie6zJ5RDAL00fgz37I",
                 "Content-type": "application/json"
@@ -53,7 +53,7 @@ function MovieDetails() {
         fetchFilms();
         fetchComments()
         console.log(data2)
-    }, [])
+    }, [movieId])
 
 
     return (
